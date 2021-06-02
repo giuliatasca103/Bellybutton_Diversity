422eec4d783771401209fe9d04e37f2fd26a88a5
Giulia Tasca <giuliatasca@Giulias-MacBook-Pro.local>
2021-06-02 12:37:37 -0700

    adding files

--- chart.js
+++ chart.js
@@ -1,34 +1,34 @@
-function init() {
-  // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
+// function init() {
+//   // Grab a reference to the dropdown select element
+//   var selector = d3.select("#selDataset");
 
-  // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((data) => {
-    var sampleNames = data.names;
+//   // Use the list of sample names to populate the select options
+//   d3.json("samples.json").then((data) => {
+//     var sampleNames = data.names;
 
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
+//     sampleNames.forEach((sample) => {
+//       selector
+//         .append("option")
+//         .text(sample)
+//         .property("value", sample);
+//     });
 
-    // Use the first sample from the list to build the initial plots
-    var firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-  });
-}
+//     // Use the first sample from the list to build the initial plots
+//     var firstSample = sampleNames[0];
+//     buildCharts(firstSample);
+//     buildMetadata(firstSample);
+//   });
+// }
 
-// Initialize the dashboard
-init();
+// // Initialize the dashboard
+// init();
 
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  buildMetadata(newSample);
-  buildCharts(newSample);
+// function optionChanged(newSample) {
+//   // Fetch new data each time a new sample is selected
+//   buildMetadata(newSample);
+//   buildCharts(newSample);
   
-}
+// }
 
 // Demographics Panel 
 function buildMetadata(sample) {
@@ -157,3 +157,35 @@ function buildCharts(sample) { // sample is going to come through as a string so
     // Plotly.newPlot("gauge", gaugeData, gaugeLayout);
   });
 };
+
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("samples.json").then((data) => {
+    var sampleNames = data.names;
+
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    var firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+};
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildMetadata(newSample);
+  buildCharts(newSample);
+  
+};
+
+// Initialize the dashboard
+init();
