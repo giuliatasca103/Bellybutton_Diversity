7ffbe2d9890c4ba1758a4412453cdeeaa9de8198
Giulia Tasca <giuliatasca@Giulias-MacBook-Pro.local>
2021-06-02 12:46:39 -0700

    adding files

--- chart.js
+++ chart.js
@@ -1,35 +1,3 @@
-// function init() {
-//   // Grab a reference to the dropdown select element
-//   var selector = d3.select("#selDataset");
-
-//   // Use the list of sample names to populate the select options
-//   d3.json("samples.json").then((data) => {
-//     var sampleNames = data.names;
-
-//     sampleNames.forEach((sample) => {
-//       selector
-//         .append("option")
-//         .text(sample)
-//         .property("value", sample);
-//     });
-
-//     // Use the first sample from the list to build the initial plots
-//     var firstSample = sampleNames[0];
-//     buildCharts(firstSample);
-//     buildMetadata(firstSample);
-//   });
-// }
-
-// // Initialize the dashboard
-// init();
-
-// function optionChanged(newSample) {
-//   // Fetch new data each time a new sample is selected
-//   buildMetadata(newSample);
-//   buildCharts(newSample);
-  
-// }
-
 // Demographics Panel 
 function buildMetadata(sample) {
   d3.json("samples.json").then((data) => {
