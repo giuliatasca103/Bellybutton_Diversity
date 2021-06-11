de757b8ddc7865b27671f0943c55a8e8a303669b
Giulia Tasca <giuliatasca@Giulias-MacBook-Pro.local>
2021-06-11 13:58:40 -0700

    add files

--- chart.js
+++ chart.js
@@ -92,36 +92,36 @@ function buildCharts(sample) { // sample is going to come through as a string so
 
     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
-    // var gaugeData = [
-      // {
-      // domain: { x: [0, 1], y: [0, 1] },
-      // value: wfreq,
-      // title: {text: 'Belly Button Washing Frequency'},
-      // type: "indicator",
+    var gaugeData = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: wfreq,
+        title: {text: 'Belly Button Washing Frequency'},
+        type: "indicator",
 
-      // mode: "gauge+number",
-      // gauge: { axis: { range: [null, 9] },
-              //  steps: [
-                //  {range: [0, 1], color: "white"},
-                //  {range: [1, 2], color: "white"},
-                //  {range: [2, 3], color: "white"},
-                //  {range: [3, 4], color: "white"},
-                //  {range: [4, 5], color: "white"},
-                //  {range: [5, 6], color: "white"},
-                //  {range: [6, 7], color: "white"},
-                //  {range: [7, 8], color: "white"},
-                //  {range: [8, 9], color: "white"}
-              //  ]}
-      // }
-    // ];
+        mode: "gauge+number",
+        gauge: { axis: { range: [null, 9] },
+              steps: [
+              {range: [0, 1], color: "white"},
+              {range: [1, 2], color: "white"},
+              {range: [2, 3], color: "white"},
+              {range: [3, 4], color: "white"},
+              {range: [4, 5], color: "white"},
+              {range: [5, 6], color: "white"},
+              {range: [6, 7], color: "white"},
+              {range: [7, 8], color: "white"},
+              {range: [8, 9], color: "white"}
+                ]}
+      }
+    ];
 
-    // var gaugeLayout = {
-        // width: 700,
-        // height: 600,
-        // margin: { t: 20, b: 40, l:100, r:100 }
-    // };
+    var gaugeLayout = {
+      width: 700,
+      height: 600,
+      margin: { t: 20, b: 40, l:100, r:100 }
+     };
 
-    // Plotly.newPlot("gauge", gaugeData, gaugeLayout);
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
   });
 };
 
